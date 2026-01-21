df95894c0a7f6c7e89d58fba79df6a89f5632aa4
Light-Wave <66904273+Light-Wave@users.noreply.github.com>
2026-01-21 16:23:58 +0100

    More WIP

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,55 @@
+import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+import Lipsum from "node-lipsum";
+import { testUserData } from "./seed_data";
 
-async function generateUsers() {}
+const lipsum = new Lipsum();
 
-async function generatePosts() {}
+async function generateUsers() {
+  // If we are in production, remove all test users instead
+  if (process.env.NODE_ENV === "production") {
+    await prisma.user.deleteMany({
+      where: { email: { endsWith: "@testing.com" } },
+    });
+    return;
+  }
+
+  const users = await prisma.user.findMany({
+    where: { email: { endsWith: "@testing.com" } },
+  });
+  for (const user of testUserData) {
+    const userExists = users.find((u) => u.email === user.email);
+    if (!userExists) {
+      await auth.api.createUser({
+        body: user,
+      });
+      prisma.user
+        .updateMany({
+          where: { email: user.email },
+          data: { emailVerified: true },
+        })
+        .catch((e) => {
+          console.error("Failed to verify email for user: ", user.email, e);
+        });
+    }
+  }
+}
+
+async function generatePosts() {
+  const postCount = await prisma.post.count();
+  for (let i = postCount; i < 20; i++) {
+    await prisma.post.create({
+      data: {
+        title: lipsum.getText({
+          amount: Math.floor(Math.random() * 10) + 1,
+          what: "words",
+        }),
+        content: `This is the content for sample post ${i + 1}.`,
+        published: true,
+      },
+    });
+  }
+}
 
 async function main() {
   await generateUsers();
---
+++ prisma/seed_data.ts
@@ -0,0 +1,45 @@
+export const testUserData: { email: string; name: string; password: string }[] =
+  [
+    {
+      email: "user@testing.com",
+      name: "Test User",
+      password: "password1234",
+    },
+    {
+      email: "subscribed_user@testing.com",
+      name: "Test Subscribed User",
+      password: "password1234",
+    },
+    {
+      email: "admin@testing.com",
+      name: "Test Admin",
+      password: "password1234",
+    },
+    {
+      email: "writer@testing.com",
+      name: "Test Writer",
+      password: "password1234",
+    },
+    {
+      email: "editor@testing.com",
+      name: "Test Editor",
+      password: "password1234",
+    },
+  ];
+
+export const clickbait_suffixes: string[] = [
+  " - Then everything went wrong.",
+  " - Could be illegal",
+  ", then the unexpected happened.",
+  ", thousands in danger!",
+  " - What it means to you.",
+  " - The secrets revealed!",
+  " - Terrifying accounts from witnesses.",
+];
+export const clickbait_prefixes: string[] = [
+  "Uncertain future - ",
+  "Trump warns ",
+  "Doctors warns: ",
+  "The big question: ",
+  "Team Better reveals: ",
+];
