2261239f0b53bffce372f894712e345210f876c6
JollyRoger4u <petterssonroger@hotmail.com>
2026-01-21 11:21:41 +0100

    First commit for landing page
    start of a very very basic landing page
    Minimal styling and content
    Only placeholders, no functionality at all

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,141 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <p>Hello world </p>
+    <div className="">
+      <main className="">
+        {/* breaking news ticker section*/}
+        <section>
+          <p className="bg-black text-white text-center">
+            Breaking news: <br />
+            Stupid people still being stupid
+          </p>
+        </section>
+        {/* Main article section (editors choice? most popular? most recent?) */}
+        <section>
+          <Card>
+            <div className="bg-gray-200 h-50">
+              <p>image here</p>
+            </div>
+            <div className="m-auto grid grid-cols-3">
+              <p className="">Author |</p>
+              <p className="">posted |</p>
+              <p className="">read time</p>
+            </div>
+            <h2 className="text-xl font-bold text-center">Article Title</h2>
+            <p className="text-gray-600">
+              Article short version goes here. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Laborum vero quis asperiores labore
+              impedit, tempore, voluptates, ipsam accusantium consequuntur
+              facere beatae neque sapiente officia aliquid.
+            </p>
+            <CardFooter>
+              <p className="">social links ❌👍🔗</p>
+              <Button>Full article</Button>
+            </CardFooter>
+          </Card>
+        </section>
+        {/* Trending articles section (most popular? recommended to user?) */}
+        <section>
+          <h2 className="font-bold gap-0 text-center">Hot and trending</h2>
+          <div>
+            <h2 className="font-bold gap-0">Article Title</h2>
+            <p className="text-gray-600">
+              Article super short version goes here. Lorem ipsum dolor, sit amet
+              consectetur
+            </p>
+            <div className="w-full flex justify-end">
+              <Button className="p-2">Full article</Button>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold gap-0">Article Title</h2>
+            <p className="text-gray-600">
+              Article super short version goes here. Lorem ipsum dolor, sit amet
+              consectetur
+            </p>
+            <div className="w-full flex justify-end">
+              <Button className="p-2">Full article</Button>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold gap-0">Article Title</h2>
+            <p className="text-gray-600">
+              Article super short version goes here. Lorem ipsum dolor, sit amet
+              consectetur
+            </p>
+            <div className="w-full flex justify-end">
+              <Button className="p-2">Full article</Button>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Card>
+            <div className="bg-gray-200 h-50">
+              <p>image here</p>
+            </div>
+            <div className="m-auto grid grid-cols-3">
+              <p className="">Author |</p>
+              <p className="">posted |</p>
+              <p className="">read time</p>
+            </div>
+            <h2 className="text-xl font-bold text-center">Article Title</h2>
+            <p className="text-gray-600">
+              Article short version goes here. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Laborum vero quis asperiores labore
+              impedit, tempore, voluptates, ipsam accusantium consequuntur
+              facere beatae neque sapiente officia aliquid.
+            </p>
+            <CardFooter>
+              <p className="">social links ❌👍🔗</p>
+              <Button>Full article</Button>
+            </CardFooter>
+          </Card>
+          <Card>
+            <div className="bg-gray-200 h-50">
+              <p>image here</p>
+            </div>
+            <div className="m-auto grid grid-cols-3">
+              <p className="">Author |</p>
+              <p className="">posted |</p>
+              <p className="">read time</p>
+            </div>
+            <h2 className="text-xl font-bold text-center">Article Title</h2>
+            <p className="text-gray-600">
+              Article short version goes here. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Laborum vero quis asperiores labore
+              impedit, tempore, voluptates, ipsam accusantium consequuntur
+              facere beatae neque sapiente officia aliquid.
+            </p>
+            <CardFooter>
+              <p className="">social links ❌👍🔗</p>
+              <Button>Full article</Button>
+            </CardFooter>
+          </Card>
+          <Card>
+            <div className="bg-gray-200 h-50">
+              <p>image here</p>
+            </div>
+            <div className="m-auto grid grid-cols-3">
+              <p className="">Author |</p>
+              <p className="">posted |</p>
+              <p className="">read time</p>
+            </div>
+            <h2 className="text-xl font-bold text-center">Article Title</h2>
+            <p className="text-gray-600">
+              Article short version goes here. Lorem ipsum dolor, sit amet
+              consectetur adipisicing elit. Laborum vero quis asperiores labore
+              impedit, tempore, voluptates, ipsam accusantium consequuntur
+              facere beatae neque sapiente officia aliquid.
+            </p>
+            <CardFooter>
+              <p className="">social links ❌👍🔗</p>
+              <Button>Full article</Button>
+            </CardFooter>
+          </Card>
+        </section>
       </main>
     </div>
   );
