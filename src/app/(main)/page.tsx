ed2a1793ba7104aaa05823881fd01bf400fb8b46
JollyRoger4u <petterssonroger@hotmail.com>
2026-01-21 12:08:57 +0100

    added subscription
    Small visual updates and added subscription section.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardFooter } from "@/components/ui/card";
-import Image from "next/image";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
         {/* breaking news ticker section*/}
-        <section>
+        <p className="text-center">fake header</p>
+        <section className="">
           <p className="bg-black text-white text-center">
             Breaking news: <br />
             Stupid people still being stupid
@@ -19,18 +19,22 @@ export default function Home() {
             <div className="bg-gray-200 h-50">
               <p>image here</p>
             </div>
-            <div className="m-auto grid grid-cols-3">
-              <p className="">Author |</p>
-              <p className="">posted |</p>
-              <p className="">read time</p>
+            <div className="flex p-2 justify-between items-center gap-4 text-sm text-gray-600">
+              <span>Author</span>
+              <span>•</span>
+              <span>Posted date</span>
+              <span>•</span>
+              <span>5 min read</span>
             </div>
-            <h2 className="text-xl font-bold text-center">Article Title</h2>
-            <p className="text-gray-600">
-              Article short version goes here. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Laborum vero quis asperiores labore
-              impedit, tempore, voluptates, ipsam accusantium consequuntur
-              facere beatae neque sapiente officia aliquid.
-            </p>
+            <CardContent>
+              <h2 className="text-xl font-bold text-center">Article Title</h2>
+              <p className="text-gray-600">
+                Article short version goes here. Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Laborum vero quis asperiores
+                labore impedit, tempore, voluptates, ipsam accusantium
+                consequuntur facere beatae neque sapiente officia aliquid.
+              </p>
+            </CardContent>
             <CardFooter>
               <p className="">social links ❌👍🔗</p>
               <Button>Full article</Button>
@@ -40,54 +44,57 @@ export default function Home() {
         {/* Trending articles section (most popular? recommended to user?) */}
         <section>
           <h2 className="font-bold gap-0 text-center">Hot and trending</h2>
-          <div>
-            <h2 className="font-bold gap-0">Article Title</h2>
+          <div className="p-2">
+            <h2 className="font-bold ">Article Title</h2>
             <p className="text-gray-600">
               Article super short version goes here. Lorem ipsum dolor, sit amet
-              consectetur
             </p>
             <div className="w-full flex justify-end">
               <Button className="p-2">Full article</Button>
             </div>
           </div>
-          <div>
-            <h2 className="font-bold gap-0">Article Title</h2>
+          <div className="p-2">
+            <h2 className="font-bold ">Article Title</h2>
             <p className="text-gray-600">
               Article super short version goes here. Lorem ipsum dolor, sit amet
-              consectetur
             </p>
             <div className="w-full flex justify-end">
               <Button className="p-2">Full article</Button>
             </div>
           </div>
-          <div>
-            <h2 className="font-bold gap-0">Article Title</h2>
+          <div className="p-2">
+            <h2 className="font-bold ">Article Title</h2>
             <p className="text-gray-600">
               Article super short version goes here. Lorem ipsum dolor, sit amet
-              consectetur
             </p>
             <div className="w-full flex justify-end">
               <Button className="p-2">Full article</Button>
             </div>
           </div>
         </section>
+        {/* Additional articles section - to be fetched from database depending on... soemthing... recommendations? date published? */}
         <section>
+          <h2 className="font-bold gap-0 text-center">Recommended for you</h2>
           <Card>
             <div className="bg-gray-200 h-50">
               <p>image here</p>
             </div>
-            <div className="m-auto grid grid-cols-3">
-              <p className="">Author |</p>
-              <p className="">posted |</p>
-              <p className="">read time</p>
+            <div className="flex p-2 justify-between items-center gap-4 text-sm text-gray-600">
+              <span>Author</span>
+              <span>•</span>
+              <span>Posted date</span>
+              <span>•</span>
+              <span>5 min read</span>
             </div>
-            <h2 className="text-xl font-bold text-center">Article Title</h2>
-            <p className="text-gray-600">
-              Article short version goes here. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Laborum vero quis asperiores labore
-              impedit, tempore, voluptates, ipsam accusantium consequuntur
-              facere beatae neque sapiente officia aliquid.
-            </p>
+            <CardContent>
+              <h2 className="text-xl font-bold text-center">Article Title</h2>
+              <p className="text-gray-600">
+                Article short version goes here. Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Laborum vero quis asperiores
+                labore impedit, tempore, voluptates, ipsam accusantium
+                consequuntur facere beatae neque sapiente officia aliquid.
+              </p>
+            </CardContent>
             <CardFooter>
               <p className="">social links ❌👍🔗</p>
               <Button>Full article</Button>
@@ -97,18 +104,22 @@ export default function Home() {
             <div className="bg-gray-200 h-50">
               <p>image here</p>
             </div>
-            <div className="m-auto grid grid-cols-3">
-              <p className="">Author |</p>
-              <p className="">posted |</p>
-              <p className="">read time</p>
+            <div className="flex p-2 justify-between items-center gap-4 text-sm text-gray-600">
+              <span>Author</span>
+              <span>•</span>
+              <span>Posted date</span>
+              <span>•</span>
+              <span>5 min read</span>
             </div>
-            <h2 className="text-xl font-bold text-center">Article Title</h2>
-            <p className="text-gray-600">
-              Article short version goes here. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Laborum vero quis asperiores labore
-              impedit, tempore, voluptates, ipsam accusantium consequuntur
-              facere beatae neque sapiente officia aliquid.
-            </p>
+            <CardContent>
+              <h2 className="text-xl font-bold text-center">Article Title</h2>
+              <p className="text-gray-600">
+                Article short version goes here. Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Laborum vero quis asperiores
+                labore impedit, tempore, voluptates, ipsam accusantium
+                consequuntur facere beatae neque sapiente officia aliquid.
+              </p>
+            </CardContent>
             <CardFooter>
               <p className="">social links ❌👍🔗</p>
               <Button>Full article</Button>
@@ -118,24 +129,48 @@ export default function Home() {
             <div className="bg-gray-200 h-50">
               <p>image here</p>
             </div>
-            <div className="m-auto grid grid-cols-3">
-              <p className="">Author |</p>
-              <p className="">posted |</p>
-              <p className="">read time</p>
+            <div className="flex p-2 sm:justify-between items-center gap-4 text-sm text-gray-600">
+              <span>Author</span>
+              <span>•</span>
+              <span>Posted date</span>
+              <span>•</span>
+              <span>5 min read</span>
             </div>
-            <h2 className="text-xl font-bold text-center">Article Title</h2>
-            <p className="text-gray-600">
-              Article short version goes here. Lorem ipsum dolor, sit amet
-              consectetur adipisicing elit. Laborum vero quis asperiores labore
-              impedit, tempore, voluptates, ipsam accusantium consequuntur
-              facere beatae neque sapiente officia aliquid.
-            </p>
+            <CardContent>
+              <h2 className="text-xl font-bold text-center">Article Title</h2>
+              <p className="text-gray-600">
+                Article short version goes here. Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Laborum vero quis asperiores
+                labore impedit, tempore, voluptates, ipsam accusantium
+                consequuntur facere beatae neque sapiente officia aliquid.
+              </p>
+            </CardContent>
             <CardFooter>
               <p className="">social links ❌👍🔗</p>
               <Button>Full article</Button>
             </CardFooter>
           </Card>
         </section>
+        {/* Newsletter subscription section */}
+        <section className="flex flex-col items-center gap-2 bg-black text-white p-2">
+          <h2 className="font-bold gap-0 text-center">SUBSCRIBE!</h2>
+          <p>
+            Stay updated with the latest articles and news. Subscribe to our
+            newsletter!
+          </p>
+          <p className="italic">
+            this action is mandatory under world law enforcement. Sign up now or
+            else face the consequences.
+          </p>
+          <input
+            className="border p-2"
+            id="subMail"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <Button className="p-2 bg-blue-500 text-white">Subscribe</Button>
+        </section>
+        <p className="text-center">fake footer</p>
       </main>
     </div>
   );
