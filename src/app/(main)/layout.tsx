423e4a41eee2be9ec6f8d558f027911bc21bd80b
romasweden2023-alt <romasweden2023@gmail.com>
2026-02-09 10:56:22 +0100

    Revert "Little updation done in admin dashboard"
    This reverts commit 10563467d4a353a19117f86d9c66e73a7a183fb0.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -44,7 +44,8 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} ${goudyBookletter1911.variable} ${cinzel.variable} dark`}
+      className={`${geistSans.variable} ${geistMono.variable} light
+      `}
     >
       <body className="min-h-screen flex flex-col overflow-x-hidden">
         <Header />
