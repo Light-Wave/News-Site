9859e3e0d2c0a589aeea90df101f7e5dfc26df06
Rallehe <Rasmushedin@live.com>
2026-02-18 09:21:26 +0100

    Redirects to homepage if not signed in or valid role

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -3,8 +3,23 @@ import { NewsStats } from "@/components/dashboard/StatCards";
 import { UserModeration } from "@/components/dashboard/UserModeration";
 import { ContentPerformance } from "@/components/dashboard/ContentPerformance";
 import { getDashboardData } from "@/actions/data";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
+  const allowedRoles = ["admin", "writer", "editor"];
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) redirect("/");
+
+  if (!session.user.role || !allowedRoles.includes(session.user.role)) {
+    redirect("/");
+  }
+
   const { statsData, latestArticles, topArticles, users } =
     await getDashboardData();
 
