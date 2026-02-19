2aadfd038ca47ce3bb661bccd599ab08c32221fe
Rallehe <115425871+Rallehe@users.noreply.github.com>
2026-02-19 08:47:57 +0100

    Access (#43)
    * Redirects to homepage if not signed in or valid role
    
    * Changed to one liner
    
    * Updating to a util function
    
    Too much dupe code
    
    * Updating redirect util function
    
    * Some kind of access control now implemented
    
    * Fixing copilots recommendations
    
    * Moving some utils functions to server-utils

--- src/app/admin/dashboard/articles/[id]/page.tsx
+++ src/app/admin/dashboard/articles/[id]/page.tsx
@@ -2,12 +2,14 @@ import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import EditArticleForm from "../edit-articles-form";
 import { getAllCategories } from "@/actions/category";
+import { redirectControl } from "@/actions/server-utils";
 
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
+  await redirectControl(["admin", "writer", "editor"], "/admin/dashboard");
   const { id } = await params;
 
   const article = await prisma.article.findUnique({
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import { TrendsChart, TopArticles } from "@/components/dashboard/Charts";
+import { getDashboardData } from "@/actions/data";
+import { redirectControl } from "@/actions/utils";
+import { TopArticles, TrendsChart } from "@/components/dashboard/Charts";
+import { ContentPerformance } from "@/components/dashboard/ContentPerformance";
 import { NewsStats } from "@/components/dashboard/StatCards";
 import { UserModeration } from "@/components/dashboard/UserModeration";
-import { ContentPerformance } from "@/components/dashboard/ContentPerformance";
-import { getDashboardData } from "@/actions/data";
 
 export default async function DashboardPage() {
+  await redirectControl(["admin", "writer", "editor"], "/");
+
   const { statsData, latestArticles, topArticles, users } =
     await getDashboardData();
 
