fefbf9723a569c42d204a78d13d2cf0a9162de58
Rallehe <115425871+Rallehe@users.noreply.github.com>
2026-02-23 09:08:54 +0100

    Added file upload for image (#51)
    * Added file upload for image
    
    Got a bit in to it and cleaned up in some older code I wrote
    
    * Update with copilots recommendations
    
    * Small final changes

--- src/app/admin/dashboard/articles/create/page.tsx
+++ src/app/admin/dashboard/articles/create/page.tsx
@@ -1,7 +1,10 @@
+import { getAllCategories } from "@/actions/category";
 import { redirectControl } from "@/actions/server-utils";
-import CreateArticlePage from "@/components/create-article/page";
+import CreateArticleForm from "@/components/create-article/CreateArticleForm";
 
 export default async function Page() {
   await redirectControl(["admin", "writer"], "/admin/dashboard");
-  return <CreateArticlePage />;
+
+  const categories = await getAllCategories();
+  return <CreateArticleForm categories={categories} />;
 }
